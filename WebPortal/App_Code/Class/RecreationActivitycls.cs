cd6a07dc06535bc59d13a3c7591d2d8df7608b4f
ArtiInf <k.aarti@infinity-data.com>
2026-09-23 07:32:30 -0400

    Add New Cls for Recreation Activites page

---
+++ WebPortal/App_Code/Class/RecreationActivitycls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPortal.App_Code.Class
+{
+    public class RecreationActivitycls
+    {
+        public int RecreationActivityId { get; set; }
+        public string RecreationActivity { get; set; }
+    }
+}
